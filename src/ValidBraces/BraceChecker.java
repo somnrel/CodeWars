53fdb77f75690e0db7bbf04e4d729ddad1721fff
kmv <misha.k4726@gmail.com>
2021-09-25 23:16:29 +0700

    <6 kyu> Valid Braces - НЕ СДЕЛАЛ!

---
+++ src/ValidBraces/BraceChecker.java
@@ -0,0 +1,16 @@
+package ValidBraces;
+
+public class BraceChecker {
+    public boolean isValid(String braces) {
+        String s = braces;
+
+        /*Хз как, подумай потом*/
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BraceChecker s = new BraceChecker();
+        System.out.println(s.isValid("{}{}{}"));
+    }
+}
